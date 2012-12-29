84b89e3890e52eb4e9ed2bb92e4dd34c16e14223
John-Peter Etcheber <jetcheber@gmail.com>
2012-12-28 17:20:08 -0800

    Changing User Agent

--- wikititle.php
+++ wikititle.php
@@ -4,7 +4,7 @@ $art_day = date("F_j,_Y");
 //echo date("F_j,_Y");
 $ch = curl_init("http://en.wikipedia.org/w/api.php?format=json&action=query&titles=Wikipedia:Today%27s_featured_article/". $art_day ."&prop=revisions&rvprop=content");
 curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_USERAGENT, 'User-Agent: Geeklet/1.0');
+curl_setopt($ch, CURLOPT_USERAGENT, 'User-Agent: Wikipedia Geektool/1.0');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $result = json_decode(curl_exec($ch), true);
 curl_close($ch);
