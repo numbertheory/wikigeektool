f39b1e23116ae36d0ea5315dfaa6cab7de2b1dc3
John-Peter Etcheber <jetcheber@gmail.com>
2013-01-08 01:24:24 -0800

    First character of title to uppercase

--- wikititle.php
+++ wikititle.php
@@ -13,5 +13,6 @@ $page_id = array_keys($result['query']['pages']);
 $page_title = $result['query']['pages'][$page_id[0]]['revisions'][0]['*'];
 $b_bracket = strpos($page_title, '\'\'\'[[');
 $e_bracket = strpos(substr($page_title, $b_bracket), ']]\'\'\'');
-echo substr($page_title, ($b_bracket + 5), ($e_bracket - 5));
+$title = substr($page_title, ($b_bracket + 5), ($e_bracket - 5));
+echo ucfirst($title);
 ?>
